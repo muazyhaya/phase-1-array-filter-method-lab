48d59d82aecff32d93f24e22886c9183832361b2
Muaz Yhaya <muazyhaya@outlook.com>
2021-07-23 14:25:03 -0700

    commit tree a50a13eaf282151bf39bdb5b1267855135b255d3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(el => el.toUpperCase() === name.toUpperCase())
+}
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(el => el.startsWith(letters));
+}   
+
+function matchName(drivers, name) {
+    return drivers.filter(el => el.name === name)
+}
+
